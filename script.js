52cf44122206e4930e1a1e6f3ac659f48ef01930
Juliekodehode <julie.kodehode@gmail.com>
2023-11-27 15:06:11 +0100

    Second commit

--- script.js
+++ script.js
@@ -57,6 +57,88 @@ for (let index = 0; index < task_four_images.length; index++) {
 	});
 }
 
-// Task five
+// Task five// Task five
+let task_five_form = document.querySelector("#task_five_form");
+
+function submitName() {
+	// Prevent the default form submission
+	// OBS: "event" is deprecated, still works, but the strike through means its not necessary the correct use. the shorthand "e" is working, but I like using fullterm.
+	event.preventDefault();
+
+	// Get the input value
+	let task_five_inputName = document.querySelector("#task_five_inputName").value;
+	console.log(task_five_inputName);
+
+	let task_five_p = document.querySelector("#task_five_p");
+	task_five_p.textContent = task_five_inputName;
+
+	// BONUS:
+	// Check if the task_five_inputName is empty or undefined, and define a response for either options.
+	// if (!task_five_inputName) {
+	// 	console.log("Please write a name!");
+
+	// 	// Display the input value in the paragraph with id task_five_p
+	// 	let task_five_p = document.querySelector("#task_five_p");
+	// 	task_five_p.textContent = "Vennligst skriv et navn";
+	// } else {
+	// 	console.log("Gosh what a name!");
+
+	// 	// Display the input value in the paragraph with id task_five_p
+	// 	let task_five_p = document.querySelector("#task_five_p");
+	// 	task_five_p.textContent = "Navnet ditt er: " + task_five_inputName;
+	// }
+}
+
+// Add event listener to the form
+let task_five_inputSubmit = document.querySelector("#task_five_inputSubmit");
+task_five_inputSubmit.addEventListener("click", submitName);
 
 // Task six
+let task_six_form = document.querySelector("#task_six_form");
+
+function submitForm() {
+	// Prevent the default form submission
+	// OBS: "event" is deprecated, still works, but the strike through means its not necessary the correct use. the shorthand "e" is working, but I like using fullterm.
+	event.preventDefault();
+
+	// Get input values to all the inputs
+	let task_six_inputName = document.querySelector("#task_six_inputName").value;
+	console.log(task_six_inputName);
+
+	let task_six_inputEpost = document.querySelector("#task_six_inputEpost").value;
+	console.log(task_six_inputEpost);
+
+	let task_six_inputPassword = document.querySelector("#task_six_inputPassword").value;
+	console.log(task_six_inputPassword);
+
+	let task_six_inputPasswordReminder = document.querySelector(
+		"#task_six_inputPasswordReminder"
+	).value;
+	console.log(task_six_inputPasswordReminder);
+
+	// Get the ul element
+	let task_six_ul = document.querySelector("#task_six_ul");
+
+	// Create li elements for each input
+	let nameLi = document.createElement("li");
+	nameLi.textContent = task_six_inputName;
+
+	let epostLi = document.createElement("li");
+	epostLi.textContent = task_six_inputEpost;
+
+	let passwordLi = document.createElement("li");
+	passwordLi.textContent = task_six_inputPassword;
+
+	let reminderLi = document.createElement("li");
+	reminderLi.textContent = task_six_inputPasswordReminder;
+
+	// Append the li elements to the ul
+	task_six_ul.appendChild(nameLi);
+	task_six_ul.appendChild(epostLi);
+	task_six_ul.appendChild(passwordLi);
+	task_six_ul.appendChild(reminderLi);
+}
+
+// Add event listener to the form
+let task_six_inputSubmit = document.querySelector("#task_six_inputSubmit");
+task_six_inputSubmit.addEventListener("click", submitForm);
